ad156a7cbc9fc0e8c1223b23748e1329c67bbd12
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-06-22 13:15:57 -0700

    Create curriculum router + model files

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,10 @@
 export default function App() {
     return (
         <>
-            <div className='bg-red-100'>
-                hifasdf
+            <div className='h-screen w-screen bg-white'>
+                <div className='flex justify-center items-center h-full'>
+                    <h1 className='text-base text-center'>upload</h1>
+                </div>
             </div>
         </>
     )
