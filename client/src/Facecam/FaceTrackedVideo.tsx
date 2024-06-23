ed0b0ac9a83baddf496cb687a9909271a8f69f37
NicholasLe04 <Nicholasl.le224@gmail.com>
2024-06-22 21:02:27 -0700

    Add facial expression detection

---
+++ client/src/Facecam/FaceTrackedVideo.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useRef } from "react";
+
+type FaceTrackedVideoProps = {
+  className?: string;
+  onVideoReady: (video: HTMLVideoElement) => void;
+  width: number;
+  height: number;
+};
+
+export function FaceTrackedVideo({ className, onVideoReady, width, height }: FaceTrackedVideoProps) {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  className = className || "";
+
+  useEffect(() => {
+    const videoElement = videoRef.current;
+    if (!videoElement) {
+      console.error("Missing video element");
+      return;
+    }
+    onVideoReady(videoElement);
+  }, []);
+
+  const canvasElement = canvasRef.current;
+  const videoElement = videoRef.current;
+  const graphics = canvasElement?.getContext("2d");
+
+  if (!canvasElement) {
+    console.info("Missing canvasElement");
+  }
+  if (!videoElement) {
+    console.info("Missing videoElement");
+  }
+  if (!graphics) {
+    console.info("Missing graphics");
+  }
+
+  if (canvasElement && videoElement && graphics) {
+    canvasElement.width = videoElement.width = width;
+    canvasElement.height = videoElement.height = height;
+    graphics.clearRect(0, 0, canvasElement.width, canvasElement.height);
+  }
+
+  return (
+      <div className={`relative h-[200px] w-full overflow-hidden rounded-lg border border-neutral-300 bg-black align-top shadow md:h-[355px] md:w-[500px] ${className}`}>
+        <video className="absolute -scale-x-[1]" ref={videoRef} autoPlay playsInline></video>
+        <canvas className="absolute" ref={canvasRef}></canvas>
+    </div>
+  );
+}
---
+++ client/src/Facecam/FaceWidgets.tsx
@@ -0,0 +1,224 @@
+import { Emotion } from "../lib/data/emotion";
+import { useEffect, useRef, useState } from "react";
+
+import { FacePrediction } from "../lib/data/facePrediction";
+import { FaceTrackedVideo } from "./FaceTrackedVideo";
+import { VideoRecorder } from "../lib/media/videoRecorder";
+import { blobToBase64 } from "../lib/utilities/blobUtilities";
+
+
+export default function FaceWidgets() {
+  const socketRef = useRef<WebSocket | null>(null);
+  const recorderRef = useRef<VideoRecorder | null>(null);
+  const photoRef = useRef<HTMLCanvasElement | null>(null);
+  const mountRef = useRef(true);
+  const recorderCreated = useRef(false);
+  const numReconnects = useRef(0);
+  const [status, setStatus] = useState("");
+  const maxReconnects = 3;
+
+  useEffect(() => {
+    console.log("Mounting component");
+    mountRef.current = true;
+    console.log("Connecting to server");
+    connect();
+
+    return () => {
+      console.log("Tearing down component");
+      stopEverything();
+    };
+  }, []);
+
+  function connect() {
+    const socket = socketRef.current;
+    if (socket && socket.readyState === WebSocket.OPEN) {
+      console.log("Socket already exists, will not create");
+    } else {
+      const baseUrl = "wss://api.hume.ai";
+      const endpointUrl = `${baseUrl}/v0/stream/models`;
+      const socketUrl = `${endpointUrl}?apikey=${import.meta.env.VITE_HUME_API_KEY}`;
+      console.log(`Connecting to websocket... (using ${endpointUrl})`);
+      setStatus(`Connecting to server...`);
+
+      const socket = new WebSocket(socketUrl);
+
+      socket.onopen = socketOnOpen;
+      socket.onmessage = socketOnMessage;
+      socket.onclose = socketOnClose;
+      socket.onerror = socketOnError;
+
+      socketRef.current = socket;
+    }
+  }
+
+  async function socketOnOpen() {
+    console.log("Connected to websocket");
+    setStatus("Connecting to webcam...");
+    if (recorderRef.current) {
+      console.log("Video recorder found, will use open socket");
+      await capturePhoto();
+    } else {
+      console.warn("No video recorder exists yet to use with the open socket");
+    }
+  }
+
+  async function socketOnMessage(event: MessageEvent) {
+    setStatus("");
+    const response = JSON.parse(event.data);
+    // console.log("Got response", response);
+    const predictions: FacePrediction[] = response.face?.predictions || [];
+    const warning = response.face?.warning || "";
+    const error = response.error;
+    if (error) {
+      setStatus(error);
+      console.error(error);
+      stopEverything();
+      return;
+    }
+
+    if (predictions.length === 0) {
+      setStatus(warning.replace(".", ""));
+    }
+
+    predictions.forEach(async (pred: FacePrediction, dataIndex: number) => {
+      if (dataIndex === 0) {
+        const newEmotions = pred.emotions;
+        newEmotions.forEach((emotion: Emotion) => {
+            if (emotion.score > 0.7) {
+                console.log("Bros " + emotion.name);
+            }
+        });
+      }
+    });
+
+    await capturePhoto();
+  }
+
+  async function socketOnClose(event: CloseEvent) {
+    console.log("Socket closed");
+
+    if (mountRef.current === true) {
+      setStatus("Reconnecting");
+      console.log("Component still mounted, will reconnect...");
+      connect();
+    } else {
+      console.log("Component unmounted, will not reconnect...");
+    }
+  }
+
+  async function socketOnError(event: Event) {
+    console.error("Socket failed to connect: ", event);
+    if (numReconnects.current >= maxReconnects) {
+      setStatus(`Failed to connect to the Hume API (prod).
+      Please log out and verify that your API key is correct.`);
+      stopEverything();
+    } else {
+      numReconnects.current++;
+      console.warn(`Connection attempt ${numReconnects.current}`);
+    }
+  }
+
+  function stopEverything() {
+    console.log("Stopping everything...");
+    mountRef.current = false;
+    const socket = socketRef.current;
+    if (socket) {
+      console.log("Closing socket");
+      socket.close();
+      socketRef.current = null;
+    } else {
+      console.warn("Could not close socket, not initialized yet");
+    }
+    const recorder = recorderRef.current;
+    if (recorder) {
+      console.log("Stopping recorder");
+      recorder.stopRecording();
+      recorderRef.current = null;
+    } else {
+      console.warn("Could not stop recorder, not initialized yet");
+    }
+  }
+
+  async function onVideoReady(videoElement: HTMLVideoElement) {
+    console.log("Video element is ready");
+
+    if (!photoRef.current) {
+      console.error("No photo element found");
+      return;
+    }
+
+    if (!recorderRef.current && recorderCreated.current === false) {
+      console.log("No recorder yet, creating one now");
+      recorderCreated.current = true;
+      const recorder = await VideoRecorder.create(videoElement, photoRef.current);
+
+      recorderRef.current = recorder;
+      const socket = socketRef.current;
+      if (socket && socket.readyState === WebSocket.OPEN) {
+        console.log("Socket open, will use the new recorder");
+        await capturePhoto();
+      } else {
+        console.warn("No socket available for sending photos");
+      }
+    }
+  }
+
+  async function capturePhoto() {
+    const recorder = recorderRef.current;
+
+    if (!recorder) {
+      console.error("No recorder found");
+      return;
+    }
+
+    const photoBlob = await recorder.takePhoto();
+    sendRequest(photoBlob);
+  }
+
+  async function sendRequest(photoBlob: Blob) {
+    const socket = socketRef.current;
+
+    if (!socket) {
+      console.error("No socket found");
+      return;
+    }
+
+    const encodedBlob = await blobToBase64(photoBlob);
+    const requestData = JSON.stringify({
+      data: encodedBlob,
+      models: {
+        face: {},
+      },
+    });
+
+    if (socket.readyState === WebSocket.OPEN) {
+      socket.send(requestData);
+    } else {
+      console.error("Socket connection not open. Will not capture a photo");
+      socket.close();
+    }
+  }
+
+  return (
+    <div>
+      <div className="md:flex">
+        <FaceTrackedVideo
+          className="mb-6"
+          onVideoReady={onVideoReady}
+          width={200}
+          height={125}
+        />
+        <div className="ml-10">
+        </div>
+        
+      </div>
+
+      <div className="pt-6">{status}</div>
+      <canvas className="hidden" ref={photoRef}></canvas>
+    </div>
+  );
+}
+
+FaceWidgets.defaultProps = {
+  onCalibrate: undefined,
+};
